1d3626240160f9674ef8c1e46f3e6243d483241f
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2021-07-04 22:18:21 +0100

    Add eslint security plugin

--- api/src/middleware/openapi/swagger-ui.ts
+++ api/src/middleware/openapi/swagger-ui.ts
@@ -1,12 +1,17 @@
 import swaggerUi from "swagger-ui-express"
 import yaml from "js-yaml"
 import fs from "fs"
+import path from "path"
 import { Application } from "express-serve-static-core"
 
 export default (app: Application): void => {
   try {
     const doc = yaml.load(
-      fs.readFileSync(__dirname + "/openapi.yml", "utf8")
+      /* eslint-disable-next-line security/detect-non-literal-fs-filename -- Safe as no value holds user input */
+      fs.readFileSync(
+        path.resolve(__dirname, "./openapi.yml"),
+        "utf8"
+      )
     ) as Record<string, unknown>
 
     app.use(
